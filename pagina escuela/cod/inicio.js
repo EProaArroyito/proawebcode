aa4c7df26f83d074035ee215abe511907c477eb2
EProaArroyito <escuelaproaarroyito@gmail.com>
2024-08-15 15:42:08 -0300

    Add files via upload

---
+++ pagina escuela/cod/inicio.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('.nav-link');
+
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+            
+            if (targetSection) {
+                targetSection.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
